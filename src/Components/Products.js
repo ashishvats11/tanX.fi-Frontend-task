24f6b7b18f9613f7bd4f501d1afdf3e0a3d8225a
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2024-02-03 17:01:57 +0530

    files created

---
+++ src/Components/Products.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import item from '../utils.js/product';
+
+const Products = () => {
+  const cat_items= item.map(items=>{
+    return (
+        
+        <div key={items.id} className=' m-3 lg:flex lg:flex-col justify-center relative'>
+        <img  src={items.img} className={`rounded-2xl hover:scale-100 transition ease-in-out duration-500 h-96 w-96 scale-90 cursor-pointer`} />
+        
+        </div>
+       
+    )
+})
+  return (
+    <div>
+      <div className='grid grid-cols-3 gap-7'>
+        <div className='flex sm:flex-row  '>
+          {cat_items}
+            
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Products
---
+++ src/utils.js/product.js
@@ -0,0 +1,40 @@
+const item =[
+    {
+        id:'1',
+        type:'tshirts',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'clothing'
+    },
+    {
+        id:'2',
+        type:'tshirts',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'clothing'
+    },
+    {
+        id:'3',
+        type:'tshirts',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'clothing'
+    },
+    {
+        id:'4',
+        type:'tshirts',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'clothing'
+    },
+    {
+        id:'5',
+        type:'tshirts',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'clothing'
+    },
+    {
+        id:'6',
+        type:'tshirts',
+        img: require('../Assets/category/women/oversized/women_oversized1.jpg'),
+        alt:'clothing'
+    },
+
+]
+module.exports = item;
